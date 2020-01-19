40c33dd6253f95f00d932006580340dae2a0e2e8
KHS <kij4300@naver.com>
2020-01-19 11:57:52 +0900

    Update TableLayoutPanel2.cs

--- TableLayoutPanel2.cs
+++ TableLayoutPanel2.cs
@@ -1,13 +1,6 @@
 ﻿// TableLayoutPanel -> 행 및 열 편집 -> 백분율
 
 using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
