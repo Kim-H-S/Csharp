dd02ac3d25727c44f6d9fbb34d830cb6c168c096
KHS <kij4300@naver.com>
2020-01-19 09:48:47 +0900

    Update SelectedRows, SelectedRows[i].Index, DataTable.Rows[i].cs

--- SelectedRows, SelectedRows[i].Index, DataTable.Rows[i].cs
+++ SelectedRows, SelectedRows[i].Index, DataTable.Rows[i].cs
@@ -3,13 +3,7 @@
 // DataTable.Rows[i]
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
