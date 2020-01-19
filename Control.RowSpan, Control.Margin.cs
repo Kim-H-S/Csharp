124295a2ccb0d0c7c4c4e1cd909bcafcfc22e7ba
KHS <kij4300@naver.com>
2020-01-19 11:54:19 +0900

    Add files via upload

---
+++ Control.RowSpan, Control.Margin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 사용자가 폼을 로드할떄마다 발생한다.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Control -> 속성 -> RowSpan: 2
+            // 해당 컨트롤이 2개의 로우를 사용할 수 있다.
+
+            // Control -> 속성 -> Margin: 0, 0, 0, 0
+            // 해당 컨트롤의 마진을 0, 0, 0, 0으로 만든다.
+        }
+
+
+
+    }
+}
